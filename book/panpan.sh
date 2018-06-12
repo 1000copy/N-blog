20bbf509bf6b82fa2ca53ce9f5b948574653075c
1000copy <1000copy@gmail.com>
2018-06-12 17:03:22 +0800

    make html book

---
+++ book/panpan.sh
@@ -0,0 +1,3 @@
+pandoc -o node.html \
+1.1nodejsinstall.md 1.2mongodbinstall.md 1.3env.md 3.1express.md 3.2router.md 3.3template.md 3.4Expressview.md 4.1devenv.md 4.2ready.md 4.3config.md 4.4designfunc.md 4.5page.md 4.6db.md 4.7dbmodel.md 5.1loginout.md 5.2article.md 5.4404.md 5.5comment.md 5.5errorpage.md 5.6log.md 5.7test.md 5.8deploy.md \
+ --toc --toc-depth=2
